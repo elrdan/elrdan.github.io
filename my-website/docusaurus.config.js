4f2a7fef786b3de2940928436ea06947e503d495
elrdan <seongo9655@gmail.com>
2024-04-03 11:23:27 +0900

    feat: 알골리아 DocSearch 기능 추가

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -11,7 +11,7 @@ const github_url = "https://github.com/elrdan"
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Elrdan',
-  tagline: 'Dinosaurs are cool',
+  tagline: '안녕하세요 Elrdan Site 방문을 환영합니다.',
   favicon: 'img/blog_logo.jpg',
 
   url: 'https://elrdan.github.io/',
@@ -39,7 +39,7 @@ const config = {
           showReadingTime: true,
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: './src/css/custom.scss',
         },
       }),
     ],
@@ -72,42 +72,36 @@ const config = {
           },
         ],
       },
+      // 알골리아 검색 설정
+      algolia: {
+        appId: "J3DX5KXBXH",
+        apiKey: '1d2a1bd27189cbc398a6a5e046c19324',
+        indexName: 'elrdanio',
+        // 옵션: 아래 문서를 참고
+        contextualSearch: true,
+
+        // 옵션: history.push 대신 window.location을 통해 탐색해야 하는 도메인을 지정합니다. 여러 문서 사이트를 크롤링하고 window.location.href를 사용하여 해당 사이트로 이동하려는 경우에 유용한 알골리아 설정입니다.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+
+        // 옵션: 알골리아에서 URL 일부를 바꿉니다. 다른 baseUrl을 사용하는 여러 배포본에 대해 같은 검색 인덱스를 사용할 경우 유용합니다. `from` 파라미터에 정규식이나 문자열을 사용할 수 있습니다. 예를 들면 localhost:3000 과 myCompany.com/docs 같은 경우입니다.
+        replaceSearchResultPathname: {
+          from: '/blog/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+
+        // 옵션: 알골리아 검색 파라미터
+        searchParameters: {},
+
+        // 옵션: 기본적으로 활성화된 검색 페이지 경로(비활성화하려면 `false`로 설정)
+        searchPagePath: 'false',
+      },
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
       colorMode: {
         // defaultMode: 'dark',        // 기본 테마 모드 설정
-      },
-      // algolia: {
-      //   // 알골리아에서 제공한 appId를 사용하세요.
-      //   appId: 'YOUR_APP_ID',
-      //
-      //   // 공개 API 키: 커밋해도 문제가 생기지 않습니다.
-      //   apiKey: 'YOUR_SEARCH_API_KEY',
-      //
-      //   indexName: 'YOUR_INDEX_NAME',
-      //
-      //   // 옵션: 아래 문서를 참고
-      //   contextualSearch: true,
-      //
-      //   // 옵션: history.push 대신 window.location을 통해 탐색해야 하는 도메인을 지정합니다. 여러 문서 사이트를 크롤링하고 window.location.href를 사용하여 해당 사이트로 이동하려는 경우에 유용한 알골리아 설정입니다.
-      //   externalUrlRegex: 'external\\.com|domain\\.com',
-      //
-      //   // 옵션: 알골리아에서 URL 일부를 바꿉니다. 다른 baseUrl을 사용하는 여러 배포본에 대해 같은 검색 인덱스를 사용할 경우 유용합니다. `from` 파라미터에 정규식이나 문자열을 사용할 수 있습니다. 예를 들면 localhost:3000 과 myCompany.com/docs 같은 경우입니다.
-      //   replaceSearchResultPathname: {
-      //     from: '/docs/', // or as RegExp: /\/docs\//
-      //     to: '/',
-      //   },
-      //
-      //   // 옵션: 알골리아 검색 파라미터
-      //   searchParameters: {},
-      //
-      //   // 옵션: 기본적으로 활성화된 검색 페이지 경로(비활성화하려면 `false`로 설정)
-      //   // searchPagePath: 'search',
-      //
-      //   //... 다른 알골리아 파라미터
-      // }
+      }
     }),
 
   // 사용자 정의 플러그인 설정
@@ -120,7 +114,8 @@ const config = {
           routeBasePath: '/project', // 이 문서 섹션의 기본 경로
           // sidebarPath: require.resolve('./sidebarsProjectDocs.js'), // 이 섹션의 사이드바 구성
         },
-      ]
+      ],
+      'docusaurus-plugin-sass'      // sass 설정
   ]
 };
 
--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -1,44 +1,33 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
+import styles from './index.module.scss';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-      <header>
-          <></>
+      <header className={styles.introContainer}>
+          <div className={styles.titleWrapper}>
+              <div className={styles.titleContent}>
+                  <span>{siteConfig.tagline}</span>
+                  <span>최대한 정확한 정보를 공유하고, 소통할 수 있는 사이트로 발전하고 싶습니다.</span>
+              </div>
+
+              <div className={styles.titleCharacter}>
+                  <img alt="깃 허브 아바타" src="https://github.com/elrdan.png"/>
+              </div>
+          </div>
       </header>
-    // <header className={clsx('hero hero--primary', styles.heroBanner)}>
-    //   <div className="container">
-    //     <Heading as="h1" className="hero__title">
-    //       {siteConfig.title}
-    //     </Heading>
-    //     <p className="hero__subtitle">{siteConfig.tagline}</p>
-    //     <div className={styles.buttons}>
-    //       <Link
-    //         className="button button--secondary button--lg"
-    //         to="/docs/intro">
-    //         Docusaurus Tutorial - 5min ⏱️
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout>
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <HomepageHeader/>
+            <main>
+                <HomepageFeatures/>
+            </main>
+        </Layout>
+    );
 }
