2644beb47bcfdc35809721d932012ba01bc12c5c
elrdan <seongo9655@gmail.com>
2024-06-16 16:07:28 +0900

    Diagram Layout 추가

---
+++ src/components/Diagram.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Diagram = ({ children }) => {
+    return (
+        <div style={{textAlign: 'center', margin: '20px 0'}}>
+            {children}
+        </div>
+    )
+}
+
+export default Diagram;
