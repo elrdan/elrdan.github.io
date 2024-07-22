5cee1ef5da5181e36a40e7200f8448bca48bd265
elrdan <163220957+elrdan@users.noreply.github.com>
2024-07-22 21:01:37 +0900

    Time: 3 ms (97.76%), Space: 44 MB (82.36%) - LeetHub

--- leetcode/0012-integer-to-roman/0012-integer-to-roman.java
+++ leetcode/0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,28 +1,16 @@
 class Solution {
     public String intToRoman(int num) {
-        // 로마 숫자와 해당 값의 맵을 생성
-        Map<Integer, String> romanMap = new LinkedHashMap<>();
-        romanMap.put(1000, "M");
-        romanMap.put(900, "CM");
-        romanMap.put(500, "D");
-        romanMap.put(400, "CD");
-        romanMap.put(100, "C");
-        romanMap.put(90, "XC");
-        romanMap.put(50, "L");
-        romanMap.put(40, "XL");
-        romanMap.put(10, "X");
-        romanMap.put(9, "IX");
-        romanMap.put(5, "V");
-        romanMap.put(4, "IV");
-        romanMap.put(1, "I");
+        // 로마 숫자와 해당 값의 배열을 생성
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
         StringBuilder roman = new StringBuilder();
 
         // 숫자를 큰 값부터 작은 값으로 나누어 로마 숫자로 변환
-        for (Map.Entry<Integer, String> entry : romanMap.entrySet()) {
-            while (num >= entry.getKey()) {
-                num -= entry.getKey();
-                roman.append(entry.getValue());
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                num -= values[i];
+                roman.append(symbols[i]);
             }
         }
 
