56a45cb09d7546c4141f7cbfe43cc99ad038b6b2
elrdan <seongo9655@gmail.com>
2024-08-04 07:19:31 +0900

    SideBar 수정

--- sidebars.js
+++ sidebars.js
@@ -1,53 +1,80 @@
 module.exports = {
   blog: [
+    // {
+    //   type: "category",
+    //   label: "프론트엔드",
+    //   items: [
+    //     "blog/algorithm/toolResource/LeetCodeSetting"
+    //   ]
+    // },
     {
       type: "category",
-      label: "알고리즘",
+      label: "백엔드",
       items: [
         {
-          type: 'category',
-          label: '툴 및 리소스',
+          type: "category",
+          label: "Java",
           items: [
-            {
-              type: "category",
-              label: "리트코드 깃허브 연동",
-              items: [
-                "blog/algorithm/toolResource/LeetCodeSetting"
-              ]
-            }
+            "blog/backend/spring-boot/tdd"
           ]
         },
-        {
-          type: "category",
-          label: "기본 개념",
-          items: [
-              "blog/algorithm/concept/PrefixSum",
-              "blog/algorithm/concept/DynamicProgramming",
-              "blog/algorithm/concept/Graph"
-          ]
-        },
-        {
-          type: "category",
-          label: "백준",
-          items: [
-              "blog/algorithm/baekjoon/p11724"
-          ]
-        },
-        {
-          type: "category",
-          label: "프로그래머스",
-          items: [
-            "blog/algorithm/programmers/P43165"
-          ]
-        },
-        {
-          type: "category",
-          label: "알고리즘 문제",
-          items: [
-            "blog/algorithm/other/PMinMax"
-          ]
-        }
+        // {
+        //   type: "category",
+        //   label: "Spring Boot",
+        //   items: [
+        //     "blog/algorithm/toolResource/LeetCodeSetting"
+        //   ]
+        // }
       ]
-    },
+    }
+    // {
+    //   type: "category",
+    //   label: "알고리즘",
+    //   items: [
+    //     {
+    //       type: 'category',
+    //       label: '툴 및 리소스',
+    //       items: [
+    //         {
+    //           type: "category",
+    //           label: "리트코드 깃허브 연동",
+    //           items: [
+    //             "blog/algorithm/toolResource/LeetCodeSetting"
+    //           ]
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       type: "category",
+    //       label: "기본 개념",
+    //       items: [
+    //           "blog/algorithm/concept/PrefixSum",
+    //           "blog/algorithm/concept/DynamicProgramming",
+    //           "blog/algorithm/concept/Graph"
+    //       ]
+    //     },
+    //     {
+    //       type: "category",
+    //       label: "백준",
+    //       items: [
+    //           "blog/algorithm/baekjoon/p11724"
+    //       ]
+    //     },
+    //     {
+    //       type: "category",
+    //       label: "프로그래머스",
+    //       items: [
+    //         "blog/algorithm/programmers/P43165"
+    //       ]
+    //     },
+    //     {
+    //       type: "category",
+    //       label: "알고리즘 문제",
+    //       items: [
+    //         "blog/algorithm/other/PMinMax"
+    //       ]
+    //     }
+    //   ]
+    // },
   ],
 };
