9257e30771434f879786c6b470238c10d1cd2882
elrdan <163220957+elrdan@users.noreply.github.com>
2024-09-03 20:38:54 +0900

    Time: 3 ms (67.3%), Space: 44.9 MB (48.52%) - LeetHub

--- leetcode/0001-two-sum/0001-two-sum.java
+++ leetcode/0001-two-sum/0001-two-sum.java
@@ -1,22 +1,22 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // 결과 인덱스를 저장할 해시맵
+        // 해시맵을 사용하여 각 숫자와 그 숫자의 인덱스를 저장합니다.
         Map<Integer, Integer> map = new HashMap<>();
-
-        for(int i = 0; i < nums.length; i++) {
+        
+        // 배열을 한 번 순회하면서 각 요소를 처리합니다.
+        for (int i = 0; i < nums.length; i++) {
+            // 목표값에서 현재 숫자를 뺀 값이 해시맵에 있는지 확인합니다.
             int complement = target - nums[i];
-
-            // 목표 값에서 현재 값을 뺀 값이 해시맵에 있는지 체크
-            if(map.containsKey(complement)) {
-                // 두 인덱스를 반환
-                return new int[]{map.get(complement), i};
+            
+            // 해시맵에 있는 경우, 두 숫자의 인덱스를 반환합니다.
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
             }
-
-            // value, index 저장
+            
+            // 해시맵에 현재 숫자와 그 인덱스를 저장합니다.
             map.put(nums[i], i);
         }
-
-        // exaclty one solution 이기때문에 의미없는 값으로 처리
-        return new int[]{0, 0};
+        
+        return new int[] {};
     }
 }
