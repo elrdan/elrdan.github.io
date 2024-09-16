7c2f56b24071ede5f702b758d98065f7a3057de2
elrdan <seongo9655@gmail.com>
2024-09-16 20:44:54 +0900

    백준 P1000 문제 풀이

---
+++ algorithm/baekjoon/class1/P1000.java
@@ -0,0 +1,20 @@
+package class1;
+
+import java.util.Scanner;
+
+/**
+ * 백준 Solved.ac class1
+ * A + B 문제 풀이
+ * https://www.acmicpc.net/problem/1000
+ */
+
+public class P1000 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();	// 첫 번째 숫자 입력
+		int b = sc.nextInt();   // 두 번째 숫자 입력
+
+		System.out.println(a + b);
+	}
+}
