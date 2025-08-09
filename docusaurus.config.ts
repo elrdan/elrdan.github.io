dc05bb10310ac618c13772e13915fcfed0f4917e
elrdan <seongo9655@gmail.com>
2025-08-09 14:04:59 +0900

    fix: blog routeBasePath 수정

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
 
-          routeBasePath: '/blog',
+          routeBasePath: 'blog',
         },
 
         // 커스텀 CSS
